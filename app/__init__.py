d954f3878e55d0b8f3863fe6bdf24be9f7685eb3
Joel Willoughby <joel.willoughby29@outlook.com>
2024-09-20 10:42:27 +0800

    Added basic SQLAlchemy DB. Added basic User table.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from app.db import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+# Import your routes and models after the app is initialized
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
 from flask import render_template, request, Flask
-
 from app import app
+from app.db import db
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the text from the 'test' input box
-        input_text = request.form.get('test')
-        print(f"Text from input box: {input_text}")  # This prints to the server console
+        username = request.form['username']
+        password = request.form['password']
+
+        # Vulnerable SQL query (SQLi)
+        query = f"SELECT * FROM user WHERE username = '{username}' AND password = '{password}'"
+        result = db.engine.execute(query).fetchone()
+
+        if result:
+            return f'Welcome {result.username}'
+        else:
+            return 'Invalid credentials'
         
     return render_template('index.html')
